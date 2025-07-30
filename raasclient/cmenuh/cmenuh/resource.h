aae0acd2435ea7d40d91d67d0c815b6e1da836fe
Erik Lundin <keg.lundin@gmail.com>
2025-07-30 14:32:07 +0200

    Translations added for cmenuh

--- raasclient/cmenuh/cmenuh/resource.h
+++ raasclient/cmenuh/cmenuh/resource.h
@@ -1,19 +1,10 @@
-/* Copyright (c) 2025 Elbitin
- *
- * This file is part of RAAS Client.
- *
- * RAAS Client is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
- *
- * RAAS Client is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with RAAS Client. If not, see <https://www.gnu.org/licenses/>.
- */
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
 // Used by CMenuH.rc
 //
 #define IDS_OPENREMOTE                  101
 #define IDS_REMOTESHORTCUT              102
+#define IDS_OPENREMOTE2                 103
 #define IDB_RAASCLIENT                  201
 
 // Next default values for new objects
