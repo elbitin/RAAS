6f59f514e1ac845d289c39e022748f6d15681141
Erik Lundin <keg.lundin@gmail.com>
2025-07-30 15:11:05 +0200

    dmenuh prepared for translation

--- raasclient/dmenuh/dmenuh/resource.h
+++ raasclient/dmenuh/dmenuh/resource.h
@@ -1,13 +1,3 @@
-/* Copyright (c) 2025 Elbitin
- *
- * This file is part of RAAS Client.
- *
- * RAAS Client is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
- *
- * RAAS Client is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with RAAS Client. If not, see <https://www.gnu.org/licenses/>.
- */
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
 // Used by DMenuH.rc
@@ -19,7 +9,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
