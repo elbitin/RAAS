70a90af5932a88aa87cef4379038b8a0b91f2dd4
Erik Lundin <keg.lundin@gmail.com>
2026-01-04 05:01:23 +0100

    Bug fix, shortcuts did not get removed on shortcutssvc update all, now fixed

--- raasserver/shortcutssvc/shortcutssvc/ShortcutsRegistrar.cs
+++ raasserver/shortcutssvc/shortcutssvc/ShortcutsRegistrar.cs
@@ -208,8 +208,9 @@ namespace Elbitin.Applications.RAAS.RAASServer.ShortcutsSvc
             }
 
             // Get contents of directory
-            String[] currentFiles = System.IO.Directory.GetFiles(targetDir);
-            String[] currentDirs = System.IO.Directory.GetDirectories(targetDir);
+            List<String> currentFiles = System.IO.Directory.GetFiles(targetDir).ToList<String>();
+            currentFiles.AddRange(GetFilesInDir(targetDir));
+            List<String> currentDirs = System.IO.Directory.GetDirectories(targetDir).ToList<String>();
 
             // Determine if directory already exist in shortcuts
             bool dirExist = false;
@@ -240,6 +241,22 @@ namespace Elbitin.Applications.RAAS.RAASServer.ShortcutsSvc
             }
 
             return true;
+        }
+
+        private List<String> GetFilesInDir(String targetDir)
+        {
+            List<String> files = new List<String>();
+            foreach(var file in this.shortcuts.Desktop.File)
+            {
+                if (file.Shortcut.ToLowerInvariant().StartsWith(targetDir.Substring(RootDirPath.Length).ToLowerInvariant()))
+                    files.Add(file.Shortcut);
+            }
+            foreach (var file in this.shortcuts.StartMenu.File)
+            {
+                if (file.Shortcut.ToLowerInvariant().StartsWith(targetDir.Substring(RootDirPath.Length).ToLowerInvariant()))
+                    files.Add(file.Shortcut);
+            }
+            return files;
         }
 
         private void RemoveExistingFile(System.IO.FileInfo fi)
